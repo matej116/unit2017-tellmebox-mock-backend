b8da0b64d1b06333b71a2b9d47b8ff316b4d2393
Matěj Polák <matejpolak42@gmail.com>
2017-04-11 14:28:52 +0200

    CORS, methods

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const app = express()
 const allowCrossDomain = function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
 
     next();
 }
